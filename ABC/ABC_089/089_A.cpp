cc80cc4e5a479e8ee47fb610fd5aa0add7628262
SyuntaFujiwara <61235023+SyuntaFujiwara@users.noreply.github.com>
2020-09-30 22:56:35 +0900

    no message

---
+++ ABC/ABC_089/089_A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <cctype>
+#define rep(i,n) for(int i=0;i<n;i++)
+using namespace std;
+using ll = long long;
+using p = pair<int,int>;
+using Graph = vector<vector<int>>;
+int main(int argc, const char * argv[]) {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin>>n;
+    cout<<n/3<<endl;
+
+    return 0;
+}
