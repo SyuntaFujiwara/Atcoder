5f7ba2ba71ab85a55bc3a87bccfe3025ff158ec5
SyuntaFujiwara <61235023+SyuntaFujiwara@users.noreply.github.com>
2020-11-24 22:51:17 +0900

    no message

---
+++ ABC/abc132/c/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)
+using namespace std;
+using ll = long long;
+using p = pair<int,int>;
+using Graph = vector<vector<int>>;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin>>n;
+    vector<int>d(n);
+    rep(i,n)cin>>d[i];
+    sort(d.begin(),d.end());
+    cout<<d[(n/2)]-d[n/2-1]<<endl;
+
+    return 0;
+}
---
+++ ABC/abc139/c/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)
+using namespace std;
+using ll = long long;
+using p = pair<int,int>;
+using Graph = vector<vector<int>>;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin>>n;
+    vector<int>h(n);
+    rep(i,n)cin>>h[i];
+    int ans=0;
+    int cnt=0;
+    rep(i,n-1){
+        if(h[i]>=h[i+1])cnt++;
+        else cnt=0;
+        chmax(ans,cnt);
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
