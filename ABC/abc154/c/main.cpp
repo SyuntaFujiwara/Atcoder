51b0b2b7cc5ef97d113acbf597c1e811eab349b8
SyuntaFujiwara <61235023+SyuntaFujiwara@users.noreply.github.com>
2020-11-25 22:13:43 +0900

    no message

---
+++ ABC/abc154/c/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)
+using namespace std;
+using ll = long long;
+using p = pair<int,int>;
+using Graph = vector<vector<int>>;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    rep(i,n)cin>>a[i];
+    map<int,int>m;
+    rep(i,n)m[a[i]]++;
+    for(const auto& [key,value]:m){
+        if(value>1){
+            cout<<"NO"<<endl;
+            return 0;
+        }
+    }
+    cout<<"YES"<<endl;
+
+    return 0;
+}
