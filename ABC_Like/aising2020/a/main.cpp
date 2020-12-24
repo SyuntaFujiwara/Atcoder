3e063c89e201bf891bf318bbbf2334a19daa363f
SyuntaFujiwara <61235023+SyuntaFujiwara@users.noreply.github.com>
2020-12-24 22:17:13 +0900

    no message

---
+++ ABC_Like/aising2020/a/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)
+using namespace std;
+using ll = long long;
+using p = pair<int,int>;
+using Graph = vector<vector<int>>;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+
+    int l,r,d;
+    cin>>l>>r>>d;
+    int ans=0;
+    for(int i=l;i<=r;i++){
+        if(i%d==0)ans++;
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
