ea74b6677dd0300613c17e344754896ff2ad3945
SyuntaFujiwara <61235023+SyuntaFujiwara@users.noreply.github.com>
2020-12-28 21:31:23 +0900

    no message2

---
+++ ABC_Like/panasonic2020/a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)
+using namespace std;
+using ll = long long;
+using p = pair<int,int>;
+using Graph = vector<vector<int>>;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+
+    int a[32]={1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+    int k;
+    cin>>k;
+    cout<<a[k-1]<<endl;
+
+    return 0;
+}
