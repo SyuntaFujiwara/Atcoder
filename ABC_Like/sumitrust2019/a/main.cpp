a44f3175958a483bc64dd0aab36eb2021710acbb
SyuntaFujiwara <61235023+SyuntaFujiwara@users.noreply.github.com>
2020-12-30 21:20:40 +0900

    no message

---
+++ ABC_Like/sumitrust2019/a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)
+using namespace std;
+using ll = long long;
+using p = pair<int,int>;
+using Graph = vector<vector<int>>;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+
+    int m1,d1,m2,d2;
+    cin>>m1>>d1>>m2>>d2;
+    if(m1+1==m2)cout<<1<<endl;
+    else cout<<0<<endl;
+
+    return 0;
+}
