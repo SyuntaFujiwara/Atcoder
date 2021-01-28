131dfb313ee18f42e32f84749f36a20aea1f4aaf
SyuntaFujiwara <syunta.fujiwara.june30@gmail.com>
2021-01-28 22:11:28 +0900

    no message

--- ABC/abc189/c/main.cpp
+++ ABC/abc189/c/main.cpp
@@ -11,39 +11,15 @@ int main(int argc, const char * argv[]) {
 
     int n;
     cin>>n;
-    vector<ll>a(n);
-    map<ll,int>b;
-    vector<ll>suma(n+1);
-    suma[0]=0;
-    rep(i, n) { 
-        cin >> a[i];
-        b[a[i]]=1;
-        suma[i+1]=suma[i]+a[i];
-    }
-    ll ans=0;
-    int front=0;
-    int back=n;
-    for(auto [key,value]:b){
-        bool flag=true;
-        int m=0;
-        int fr=front,ba,FRONT=-1,BACK=-1;
-        for (int i = front; i < back; i++) {
-            if (a[i] < key &&flag == false) {
-              fr = i;
-              flag = true;
-            }
-            else if(a[i]<key){
-                ba=i-1;
-                flag=false;
-                if (chmax(m, back - front)) {
-                  FRONT = fr;
-                  BACK = ba;
-                }
-            }
+    vector<int>a(n);
+    rep(i,n)cin>>a[i];
+    int ans=0;
+    rep(l,n){
+        int x=a[l];
+        for(int r=l;r<n;r++) {
+          chmin(x, a[r]);
+          chmax(ans, x * (r - l + 1));
         }
-        chmax(ans,m*key);
-        if(FRONT!=-1)front=FRONT+1;
-        if(BACK!=-1)back=BACK+1;
     }
     cout<<ans<<endl;
 
