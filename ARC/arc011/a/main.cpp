d8e9674b24495d8f5cee8e91a7a8fcf17b2e1c78
SyuntaFujiwara <syunta.fujiwara.june30@gmail.com>
2021-02-01 20:49:34 +0900

    no message

---
+++ ARC/arc011/a/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)
+using namespace std;
+using ll = long long;
+using p = pair<int,int>;
+using Graph = vector<vector<int>>;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+
+    int n,m,N;
+    cin>>m>>n>>N;
+    int ans=N;
+    for(;N>=m;){
+        ans+=(N/m)*n;
+        N=(N/m)*n+N%m;
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
+
