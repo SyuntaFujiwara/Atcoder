5e0c00dd05aeb85894b3b6740c4346fc78d17e78
SyuntaFujiwara <syunta.fujiwara.june30@gmail.com>
2021-02-05 21:01:46 +0900

    no message

---
+++ OtherContests/code-festival-2014-final/b/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)
+using namespace std;
+using ll = long long;
+using p = pair<int,int>;
+using Graph = vector<vector<int>>;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+
+    string s;
+    cin>>s;
+    int ans=0;
+    for(int i=0;i<s.size();i++){
+        if(i%2==0)ans+=s[i]-'0';
+        else ans-=s[i]-'0';
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
+
