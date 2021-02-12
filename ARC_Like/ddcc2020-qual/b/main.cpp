bbd97483500bf462a5ae9084ed5210f9730f55b2
SyuntaFujiwara <syunta.fujiwara.june30@gmail.com>
2021-02-12 21:35:11 +0900

    no message

--- ARC_Like/ddcc2020-qual/b/main.cpp
+++ ARC_Like/ddcc2020-qual/b/main.cpp
@@ -9,8 +9,26 @@ template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } retu
 int main(int argc, const char * argv[]) {
     ios::sync_with_stdio(false);
 
+    ll n;
+    cin>>n;
+    vector<ll>a(n);
+    rep(i,n)cin>>a[i];
+    vector<ll>sum(n);
+    sum[0]=a[0];
+    for(int i=1;i<n;i++)sum[i]=sum[i-1]+a[i];
+    ll l_sum,r_sum;
+    ll ans;
+    rep(i,n){
+        if(sum[i]>=sum[n-1]/2){
+            l_sum=sum[i];
+            r_sum=sum[n-1]-l_sum;
+            ans=l_sum-r_sum;
+            chmin(ans,sum[n-1]-sum[i-1]-sum[i-1]);
+            break;
+        }
+    }
+    cout<<ans<<endl;
     
-
     return 0;
 }
 
